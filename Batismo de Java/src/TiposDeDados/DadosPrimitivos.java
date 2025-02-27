931b99dc3521f55afa939f19d49a3d922d34a922
Leonardo Sanders <leonardosanders54@gmail.com>
2025-02-26 21:34:02 -0400

    Conclusao da Aula de Dados Primitivos

--- Batismo de Java/src/TiposDeDados/DadosPrimitivos.java
+++ Batismo de Java/src/TiposDeDados/DadosPrimitivos.java
@@ -1,4 +1,24 @@
 package TiposDeDados;
 
 public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        int: Números inteiros.
+        float: Números de ponto flutuante de precisão simples.
+        double: Números de ponto flutuante de precisão dupla.
+        char: Um único caractere.
+        boolean: Valores verdadeiros ou falsos.
+        byte: Números inteiros pequenos.
+        short: Números inteiros menores que um int.
+        Long: Números inteiros maiores que um int.
+        *
+        Objetivo da aula: criar um ninja!
+         */
+
+        int idade = 16; // Valor maximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean ninja = true;
+        Long saldoBancario = 99999L;
+    }
 }
